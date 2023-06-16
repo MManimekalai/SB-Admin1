8e4a46181bae4e7da0af5d6c16d5fef5328753bc
MManimekalai <124970075+MManimekalai@users.noreply.github.com>
2023-06-16 15:45:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from "./Component/Sidebar";
+import Dashboard from "./Component/Dashboard";
+function App() {
+  return <>
+    <div id="wrapper">
+        <Sidebar/>
+        <Dashboard/>
+    </div>
+  </>
+}
+
+export default App;
